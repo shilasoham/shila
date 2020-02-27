399a397c2c31bc5f6a6bea59db770381e898e2ef
shilasoahm <shilasoahm@gmail.com>
2020-02-27 06:22:19 +0530

    f1.r

---
+++ f1.r
@@ -0,0 +1,15 @@
+print("hi")
+setwd("C:/Users/tcet/Desktop/shila")
+getwd()
+mydata=read.csv("StudentsPerformance.csv",head=TRUE,sep=",")
+View(mydata)
+str(mydata)
+summary(mydata)
+head(mydata,6)
+col1=mydata$math.score
+str(col1)
+apply(mydata$math.score,2,sum)
+x=read.csv("C:/Users/tcet/Desktop/shila/da.csv",head=TRUE,sep=",")
+View(x)
+
+
