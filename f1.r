facea8b2fb02285120f8385caf0190b8483197b2
shilasoham <61532369+shilasoham@users.noreply.github.com>
2020-02-27 14:30:48 +0530

    Update f1.r
    new changes

--- f1.r
+++ f1.r
@@ -1,7 +1,7 @@
 print("hi")
 setwd("C:/Users/tcet/Desktop/shila")
 getwd()
-View(mydata)
+//View(mydata) new changes
 mydata=read.csv("StudentsPerformance.csv",head=TRUE,sep=",")
 View(mydata)
 str(mydata)
@@ -12,5 +12,5 @@ str(col1)
 apply(mydata$math.score,2,sum)
 x=read.csv("C:/Users/tcet/Desktop/shila/da.csv",head=TRUE,sep=",")
 View(x)
-print("shila")
+//print("shila")
 
